bde62eacf8cb70150fb2125584c27ef312c8639d
Hugo Araujo <66556677+huhulacolle@users.noreply.github.com>
2022-11-09 14:41:02 +0100

    add back

---
+++ Back/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const hostname = "0.0.0.0";
+const port = 3000;
+
+const server = express();
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017");
+
+server.use(express.urlencoded());
+server.use(express.json());
+
+
+const postRoute = require("./src/routes/postRoute");
+postRoute(server);
+
+const commentRoute = require("./src/routes/commentRoute");
+commentRoute(server);
+
+server.listen(port, hostname);
