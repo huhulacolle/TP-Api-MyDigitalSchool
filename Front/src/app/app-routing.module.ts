9b7863c1a8f3af6882edcbe19b0fcfe643317b41
Hugo Araujo <66556677+huhulacolle@users.noreply.github.com>
2022-11-09 21:08:47 +0100

    page connexion

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { TestComponent } from './components/test/test.component';
+import { LoginRegisterComponent } from './components/login-register/login-register.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginRegisterComponent },
+  { path: 'test', component: TestComponent},
+  { path: '', pathMatch: 'full', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginRegisterComponent } from './components/login-register/login-register.component';
+import { TestComponent } from './components/test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginRegisterComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
