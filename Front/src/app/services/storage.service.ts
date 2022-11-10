f876398ffb85e10a9b30fefc67c4cff5b2a7a6e3
Hugo Araujo <66556677+huhulacolle@users.noreply.github.com>
2022-11-10 11:29:00 +0100

    localstorage

--- Front/src/app/services/storage.service.ts
+++ Front/src/app/services/storage.service.ts
@@ -1,58 +1,74 @@
 import { Token } from './../interfaces/token';
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { Router } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import jwt_decode from "jwt-decode";
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StorageService {
   constructor(
-    private router: Router
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object
   ) { }
 
   clean(): void {
-    localStorage.clear();
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.clear();
+    }
   }
 
   saveUser(token: string): void {
-    localStorage.removeItem("Token");
-    localStorage.setItem("Token", token);
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.removeItem("Token");
+      localStorage.setItem("Token", token);
+    }
   }
 
   removeUser(): void {
-    localStorage.removeItem("Token");
-    this.router.navigateByUrl("/")
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.removeItem("Token");
+      this.router.navigateByUrl("/")
+    }
   }
 
   isAdmin(): boolean {
-    const token = localStorage.getItem("Token");
-    const tokenDecoded: any = jwt_decode(token as string);
-    if (tokenDecoded.role == "Admin") {
-      return true;
+    if (isPlatformBrowser(this.platformId)) {
+      const token = localStorage.getItem("Token");
+      const tokenDecoded: any = jwt_decode(token as string);
+      if (tokenDecoded.role == "Admin") {
+        return true;
+      }
+      return false
     }
     return false
 
   }
 
   getUser(): any {
-    const user = localStorage.getItem("Token");
-    if (user) {
-      return JSON.parse(user);
+    if (isPlatformBrowser(this.platformId)) {
+      const user = localStorage.getItem("Token");
+      if (user) {
+        return JSON.parse(user);
+      }
     }
 
     return {};
   }
 
   isLoggedIn(): boolean {
-    const user = localStorage.getItem("Token");
-    const helper = new JwtHelperService();
+    if (isPlatformBrowser(this.platformId)) {
+      const user = localStorage.getItem("Token");
+      const helper = new JwtHelperService();
 
-    if (user || !helper.isTokenExpired(user?.toString())) {
-      return true;
+      if (user || !helper.isTokenExpired(user?.toString())) {
+        return true;
+      }
+      return false;
     }
-    return false;
+    return false
   }
 
 }
--- Front/src/app/services/token-interceptor.service.ts
+++ Front/src/app/services/token-interceptor.service.ts
@@ -1,18 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { catchError } from 'rxjs/operators';
 import { Observable, throwError } from 'rxjs';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TokenInterceptorService implements HttpInterceptor {
-  constructor() {}
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
   intercept(
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    const token = localStorage.getItem('Token');
+    let token: string | null = "";
+
+    if (isPlatformBrowser(this.platformId)) {
+      token = localStorage.getItem('Token');
+    }
+
     if (token) {
       request = request.clone({
         setHeaders: {
